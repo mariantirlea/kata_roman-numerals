99e09be79745546e1cd3d62e5614b5f508fcb872
Marian Tirlea <marian.tirlea@pss.ro>
2020-10-30 17:10:56 +0200

    Implement logic: arabic to roman (limits: [1, 3999999])

--- src/main/java/ro/mariantirlea/kata_roman_numerals/ArabicToRoman.java
+++ src/main/java/ro/mariantirlea/kata_roman_numerals/ArabicToRoman.java
@@ -1,31 +1,125 @@
 package ro.mariantirlea.kata_roman_numerals;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class ArabicToRoman {
+
+    public static final int LIMIT_ARABIC_LOW = 1;
+//    public static final int LIMIT_ARABIC_HIGH = 3999;
+    public static final int LIMIT_ARABIC_HIGH = 3999999;
+
+    private enum Symbol {
+
+        I(1),
+        V(5),
+        X(10),
+        L(50),
+        C(100),
+        D(500),
+        M(1000),
+
+        V̅(5000),
+        X̅(10000),
+        L̅(50000),
+        C̅(100000),
+        D̅(500000),
+        M̅(1000000);
+
+        private final int value;
+
+        private static final Map<Integer, Symbol> lookup = new HashMap<>();
+
+        static {
+            for (Symbol symbol : Symbol.values()) {
+                lookup.put(symbol.getValue(), symbol);
+            }
+        }
+
+        Symbol(final int value) {
+            this.value = value;
+        }
+
+        public int getValue() { return value; }
+
+        public static Symbol get(int value) {
+            return lookup.get(Integer.valueOf(value));
+        }
+    }
 
     private ArabicToRoman(){
         throw new IllegalStateException();
     }
 
     public static String arabicToRoman(int number){
-        switch (number){
-            case 1:
-                return "I";
-            case 5:
-                return "V";
-            case 10:
-                return "X";
-            case 50:
-                return "L";
-            case 100:
-                return "C";
-            case 500:
-                return "D";
-            case 1000:
-                return "M";
-            default:
-                throw new IllegalArgumentException();
+
+        validate(number);
+
+        return extractRomanFromArabic(number);
+    }
+
+    private static String extractRomanFromArabic(int number) {
+        StringBuilder sb = new StringBuilder();
+
+        int length = getNumberOfDigits(number);
+
+        int numberTemp = number;
+        for (int i = 0; i < length; i++) {
+            double temp = Math.pow(10, (double)length - i - 1);
+
+            sb.append(getRomanSymbols((int) (numberTemp / temp), length - i));
+            numberTemp = (int) (numberTemp % temp);
         }
 
+        return sb.toString();
+    }
+
+    private static String getRomanSymbols(int number, int length) {
+
+        if(number == 0){
+            return "";
+        }
+
+        if(number <= 3){
+
+            return repeat(symbolForNumberAndLength(1, length - 1), number);
+        } else if (number == 4){
+
+            return symbolForNumberAndLength(1, length - 1) +
+                    symbolForNumberAndLength(5, length - 1);
+
+        } else if (number == 5){
+            return symbolForNumberAndLength(5, length - 1);
+
+        } else if(number <= 8){
+
+            return symbolForNumberAndLength(5, length - 1) +
+                    repeat(symbolForNumberAndLength(1, length - 1), number - 5);
+
+        } else {
+
+            return symbolForNumberAndLength(1, length - 1) +
+                    symbolForNumberAndLength(1, length);
+        }
+
+    }
+
+    private static String symbolForNumberAndLength(int number, int length) {
+        return Symbol.get((int) (number * Math.pow(10, length))).name();
+    }
+
+    private static String repeat(String symbol, int number) {
+        return new String(new char[number]).replace("\0", symbol);
+    }
+
+    private static int getNumberOfDigits(int number) {
+        return (int) (Math.log10(number) + 1);
+    }
+
+    private static void validate(int number) {
+        if(number < LIMIT_ARABIC_LOW || number > LIMIT_ARABIC_HIGH){
+            throw new IllegalArgumentException();
+        }
     }
 
 }
--- src/test/java/ro/mariantirlea/kata_roman_numerals/ArabicToRomanTest.java
+++ src/test/java/ro/mariantirlea/kata_roman_numerals/ArabicToRomanTest.java
@@ -1,13 +1,15 @@
 package ro.mariantirlea.kata_roman_numerals;
 
+import com.sun.xml.internal.ws.wsdl.writer.document.StartWithExtensionsType;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ArabicToRomanTest {
+class ArabicToRomanTest {
 
     @Test
-    public void testTrivialNumbers(){
+    void testTrivialNumbers(){
         assertThat(ArabicToRoman.arabicToRoman(1)).isEqualTo("I");
         assertThat(ArabicToRoman.arabicToRoman(5)).isEqualTo("V");
         assertThat(ArabicToRoman.arabicToRoman(10)).isEqualTo("X");
@@ -17,4 +19,67 @@ public class ArabicToRomanTest {
         assertThat(ArabicToRoman.arabicToRoman(1000)).isEqualTo("M");
     }
 
+    @Test
+    void testLimits() {
+
+        assertThatThrownBy(() -> ArabicToRoman.arabicToRoman(0)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> ArabicToRoman.arabicToRoman(-20)).isInstanceOf(IllegalArgumentException.class);
+//        assertThatThrownBy(() -> ArabicToRoman.arabicToRoman(4000)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> ArabicToRoman.arabicToRoman(4000000)).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @Test
+    void testOneDigit(){
+        assertThat(ArabicToRoman.arabicToRoman(2)).isEqualTo("II");
+        assertThat(ArabicToRoman.arabicToRoman(3)).isEqualTo("III");
+        assertThat(ArabicToRoman.arabicToRoman(4)).isEqualTo("IV");
+        assertThat(ArabicToRoman.arabicToRoman(6)).isEqualTo("VI");
+        assertThat(ArabicToRoman.arabicToRoman(7)).isEqualTo("VII");
+        assertThat(ArabicToRoman.arabicToRoman(8)).isEqualTo("VIII");
+        assertThat(ArabicToRoman.arabicToRoman(9)).isEqualTo("IX");
+
+    }
+
+    @Test
+    void testTwoDigits(){
+        assertThat(ArabicToRoman.arabicToRoman(11)).isEqualTo("XI");
+        assertThat(ArabicToRoman.arabicToRoman(12)).isEqualTo("XII");
+        assertThat(ArabicToRoman.arabicToRoman(13)).isEqualTo("XIII");
+        assertThat(ArabicToRoman.arabicToRoman(14)).isEqualTo("XIV");
+        assertThat(ArabicToRoman.arabicToRoman(15)).isEqualTo("XV");
+        assertThat(ArabicToRoman.arabicToRoman(16)).isEqualTo("XVI");
+        assertThat(ArabicToRoman.arabicToRoman(17)).isEqualTo("XVII");
+        assertThat(ArabicToRoman.arabicToRoman(18)).isEqualTo("XVIII");
+        assertThat(ArabicToRoman.arabicToRoman(19)).isEqualTo("XIX");
+        assertThat(ArabicToRoman.arabicToRoman(20)).isEqualTo("XX");
+
+        assertThat(ArabicToRoman.arabicToRoman(99)).isEqualTo("XCIX");
+        assertThat(ArabicToRoman.arabicToRoman(55)).isEqualTo("LV");
+        assertThat(ArabicToRoman.arabicToRoman(40)).isEqualTo("XL");
+    }
+
+    @Test
+    void testThreeDigits(){
+        assertThat(ArabicToRoman.arabicToRoman(189)).isEqualTo("CLXXXIX");
+        assertThat(ArabicToRoman.arabicToRoman(799)).isEqualTo("DCCXCIX");
+
+    }
+
+    @Test
+    void testFourDigits(){
+        assertThat(ArabicToRoman.arabicToRoman(1998)).isEqualTo("MCMXCVIII");
+        assertThat(ArabicToRoman.arabicToRoman(2020)).isEqualTo("MMXX");
+        assertThat(ArabicToRoman.arabicToRoman(3000)).isEqualTo("MMM");
+        assertThat(ArabicToRoman.arabicToRoman(3999)).isEqualTo("MMMCMXCIX");
+
+    }
+
+    @Test
+    void testSpecial(){
+        assertThat(ArabicToRoman.arabicToRoman(1000000)).isEqualTo("M̅");
+        assertThat(ArabicToRoman.arabicToRoman(2952349)).isEqualTo("M̅M̅C̅M̅L̅MMCCCXLIX");
+        assertThat(ArabicToRoman.arabicToRoman(3999999)).isEqualTo("M̅M̅M̅C̅M̅X̅C̅MX̅CMXCIX");
+
+    }
 }

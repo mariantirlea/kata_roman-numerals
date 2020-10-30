90c2f80fc9971750bcad2398f643935c3f4bae5f
Marian Tirlea <marian.tirlea@pss.ro>
2020-10-30 17:14:52 +0200

    Change documentation and remove unused import

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # kata_roman-numerals
 Coding Dojo Handbook (Emily Bache)
+
+**Set text file encoding to UTF-8 inside Eclipse to see special characters used for values above 3999**
--- src/test/java/ro/mariantirlea/kata_roman_numerals/ArabicToRomanTest.java
+++ src/test/java/ro/mariantirlea/kata_roman_numerals/ArabicToRomanTest.java
@@ -1,10 +1,9 @@
 package ro.mariantirlea.kata_roman_numerals;
 
-import com.sun.xml.internal.ws.wsdl.writer.document.StartWithExtensionsType;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
 
 class ArabicToRomanTest {
 
